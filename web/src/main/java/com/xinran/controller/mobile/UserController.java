f7e97412e4efd5f00490ef2b40017dc12b1400d1
vavi <ghjapple@163.com>
2015-04-08 20:24:47 +0800

    delete right

--- web/src/main/java/com/xinran/controller/mobile/UserController.java
+++ web/src/main/java/com/xinran/controller/mobile/UserController.java
@@ -32,7 +32,7 @@ public class UserController {
 
         if (canSignIn) {
             HttpSession session = request.getSession(true);
-            session.getId();
+            String sessionId = session.getId();
         }
 
         // write accesToken
