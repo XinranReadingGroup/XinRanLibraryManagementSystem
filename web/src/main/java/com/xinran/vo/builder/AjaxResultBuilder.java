16d9d4f0efd0492c35f80c245dbd5902c7470d54
vavi <ghjapple@163.com>
2015-04-08 20:24:58 +0800

    delete right

---
+++ web/src/main/java/com/xinran/vo/builder/AjaxResultBuilder.java
@@ -0,0 +1,15 @@
+
+package com.xinran.vo.builder;
+
+import com.xinran.vo.AjaxResult;
+
+/**
+ * @author 高海军 帝奇 Apr 8, 2015 8:03:10 PM
+ */
+public class AjaxResultBuilder {
+
+    public static AjaxResult buildSuccessfulResult() {
+        AjaxResult ajaxResult = new AjaxResult();
+        return ajaxResult;
+    }
+}
