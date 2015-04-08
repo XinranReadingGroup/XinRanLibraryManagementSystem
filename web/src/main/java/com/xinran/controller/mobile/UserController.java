a06412d17f5e499f4532826394efa6e2788ee636
vavi <ghjapple@163.com>
2015-04-08 20:07:02 +0800

    delte right declaimed

--- web/src/main/java/com/xinran/controller/mobile/UserController.java
+++ web/src/main/java/com/xinran/controller/mobile/UserController.java
@@ -1,10 +1,4 @@
-/*
- * Copyright 1999-2014 Alibaba.com All right reserved. This software is the
- * confidential and proprietary information of Alibaba.com ("Confidential
- * Information"). You shall not disclose such Confidential Information and shall
- * use it only in accordance with the terms of the license agreement you entered
- * into with Alibaba.com.
- */
+
 package com.xinran.controller.mobile;
 
 import javax.servlet.http.HttpServletRequest;
