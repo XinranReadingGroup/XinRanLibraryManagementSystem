f963f0692efab5d262296c19c40edf2c120a0b4b
行藏 <zhangsen.z@alibaba-inc.com>
2015-05-17 17:08:56 +0800

    up

--- web/src/main/webapp/js/home.js
+++ web/src/main/webapp/js/home.js
@@ -12,7 +12,7 @@ define(function(require, exports, module) {
 
 
 
-   var shareObj = {
+   var homeObj = {
 
    		hasInit:false,
 
@@ -24,95 +24,13 @@ define(function(require, exports, module) {
 
     		if( this.hasInit === false ){
     			this.hasInit = true;
-    			this.$rootEl = $('#book-share-box-root');
-	    		this.$inputIsbnEl = this.$rootEl.find('.J-input-isbn');
-	    		this.$inputDonorEl = this.$rootEl.find('.J-input-donor');
-	    		this.$btnGetBookEl = this.$rootEl.find('.J-btn-get-book');
 
-	    		//book info
-	    		this.$bookInfoRootEl =  $('#bookinfo-box-root');
-	    		this.$btnShareBookEl = this.$bookInfoRootEl.find('.J-btn-share-book');
-	    		this.initEvents();
+    		    $('[data-toggle="tooltip"]').tooltip();
     		}
-    	},
-    	/**
-    		事件初始化
-    		@method initEvents
-    	*/
-    	initEvents:function(){
-
-    		this.$btnGetBookEl.on('click',$.proxy(function( ev ){
-    			var $el = $(ev.currentTarget);
-    			this.getBookInfo();
-    		},this));	
-
-    		this.$btnShareBookEl.on('click',$.proxy(function( ev ){
-    			alert('共享成功');
-    		},this));
-
-    		
-
-    	},
-    	/**
-    		通过isbn 获得图书信息
-    		@method getBookInfo
-    	*/
-    	getBookInfo:function(){
-    		var isbnStr = this.$inputIsbnEl.val(),
-    			getBookUrl = 'https://api.douban.com/v2/book/isbn/:' + isbnStr;
-
-    		$.ajax({
-				url: getBookUrl,
-				type:'get',
-				dataType: 'jsonp',
-				cache: false,
-				success: $.proxy(function( data ){
-					if( data && data.msg != 'book_not_found'  ){
-						this.setBookInfo( data );	
-						this.changeToBookInfoUI( true);		
-					}else{
-						alert('无法找到isbn对应的图书信息');
-					}
-				},this)
-				
-			});
-
-    	},
-		/**
-    		设置图书信息
-    		@method setBookInfo
-    	*/
-    	setBookInfo:function( bookInfo ){
-
-    		this.$bookInfoRootEl.find('.J-book-pic').attr('src', bookInfo.image  );
-    		this.$bookInfoRootEl.find('.J-book-title').html(bookInfo.title + '(' + bookInfo.subtitle + ')'  );
-    		this.$bookInfoRootEl.find('.J-author').html( bookInfo.author.join('&nbsp;|&nbsp;') );
-    		this.$bookInfoRootEl.find('.J-publisher').html( bookInfo.publisher );
-    		this.$bookInfoRootEl.find('.J-pubdate').html( bookInfo.pubdate );
-    		this.$bookInfoRootEl.find('.J-price').html( bookInfo.price );
-    		this.$bookInfoRootEl.find('.J-binding').html( bookInfo.binding );
-    		this.$bookInfoRootEl.find('.J-pubdate').html( bookInfo.pubdate );
-    		this.$bookInfoRootEl.find('.J-isbn').html( bookInfo.isbn13 || bookInfo.isbn10  );
-    		this.$bookInfoRootEl.find('.J-author_intro').html( bookInfo.author_intro );
-    		this.$bookInfoRootEl.find('.J-summary').html( bookInfo.summary  );
-
-    	},
-    	/**
-    		设置图书信息
-    		@method setBookInfo
-    	*/
-    	changeToBookInfoUI:function( isTrue ){
-    		if( isTrue === true ){
-    			this.$rootEl.hide();
-    			this.$bookInfoRootEl.show();
-    		}else{
-    			this.$rootEl.show();
-    			this.$bookInfoRootEl.hide();
-    		}
-
     	}
+    	
 
     };
 
-    module.exports = shareObj;
+    module.exports = homeObj;
 });
