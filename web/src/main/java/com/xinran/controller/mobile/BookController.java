d84689d9478ccf20c76202d660dc04224ce2f6e2
九曲 <zhuangyao.zy@alibaba-inc.com>
2015-05-21 09:46:49 +0800

    prepare book search

--- web/src/main/java/com/xinran/controller/mobile/BookController.java
+++ web/src/main/java/com/xinran/controller/mobile/BookController.java
@@ -2,6 +2,7 @@
 package com.xinran.controller.mobile;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -150,6 +151,20 @@ public class BookController {
     		fillBookInfo(records);
     	}
     	return AjaxResultBuilder.buildSuccessfulResult(records);
+    }
+    
+    
+    @RequestMapping("/book/search/{keyword}")
+    public @ResponseBody AjaxResult search(@PathVariable(value = "keyword") String keyword, HttpServletRequest request) {
+    	List<Book> books = null;
+    	Book book = bookService.findBookByISBN(keyword, true);
+    	if(book != null){
+    		books = new ArrayList<Book>();
+    		books.add(book);
+    	}else{
+    		books = bookService.queryByTitle(keyword);
+    	}
+    	return AjaxResultBuilder.buildSuccessfulResult(books);
     }
 
 	private void fillBookInfo(List<OnOffStockRecord> records) {
