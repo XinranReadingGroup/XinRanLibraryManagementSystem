4fc1bc4d60ed07d65a3ee4bf323c4e631fa492f1
freestyle21 <675861708@qq.com>
2015-06-04 19:26:24 +0800

    book detail

--- web/src/main/webapp/js/bookDetail.js
+++ web/src/main/webapp/js/bookDetail.js
@@ -59,6 +59,8 @@ define(function(require, exports, module) {
         }
         viewMoreSummary.click(function() {
             bookSummary.addClass('xy-detail-des-show-all');
+            this.hide();
+            return false;
         });
     }
 
