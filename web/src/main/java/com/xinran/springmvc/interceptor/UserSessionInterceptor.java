778e69991324aa9e08be715c06923e21f84ee81c
vavi <ghjapple@163.com>
2015-07-11 17:45:46 +0800

    add debug log

--- web/src/main/java/com/xinran/springmvc/interceptor/UserSessionInterceptor.java
+++ web/src/main/java/com/xinran/springmvc/interceptor/UserSessionInterceptor.java
@@ -14,7 +14,6 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.alibaba.fastjson.JSON;
-import com.google.common.collect.Lists;
 import com.xinran.controller.util.UserIdenetityUtil;
 import com.xinran.exception.AuthorizationException;
 
@@ -22,38 +21,16 @@ public class UserSessionInterceptor implements HandlerInterceptor {
 
      private Logger log = LoggerFactory.getLogger(this.getClass());
 
-    private static List<String> noNeedLoginURLs = Lists.newArrayList();
+    private  List<String> noNeedLoginURLs ;
 
 
 
-    // {
-    // // web 首页
-    // // noNeedLoginURLs.add("/");
-    //
-    // // mobile 首页
-    // noNeedLoginURLs.add("/mobile");
-    //
-    // noNeedLoginURLs.add("/search");
-    //
-    // noNeedLoginURLs.add("/user/session/signIn");
-    // noNeedLoginURLs.add("/user/session/signUp");
-    //
-    // noNeedLoginURLs.add("/user/signUp");
-    // noNeedLoginURLs.add("/user/signIn");
-    // noNeedLoginURLs.add("/user/signOut");
-    //
-    // noNeedLoginURLs.add("/book/isbn");
-    //
-    // }
-
-    @Override
+	@Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String requestURI = request.getRequestURI();
         
         log.debug("requestURI is {} ,noNeedLoginURLs is  ", requestURI,JSON.toJSONString(noNeedLoginURLs));
 
-        
-        
         if ("/".equals(requestURI) || "/mobile".equals(requestURI)) {
             return true;
         }
@@ -72,48 +49,7 @@ public class UserSessionInterceptor implements HandlerInterceptor {
         } else {
             return true;
         }
-        // // 白名单机制, 注册,登录,首页,搜索,数据详情,详情二维码,其他都需要登录登录
-        // String requestURI = request.getRequestURI();
-        //
-        //
-        // for (String url : noNeedLoginURLs) {
-        // // TODO 使用注解会更好点
-        // if (-1 != requestURI.indexOf(url)) {
-        // return true;
-        // }
-        //
-        // }
-        //
-        // Long currentUserIdFromSession = UserIdenetityUtil.getCurrentUserId(request);
-        // String accessTokenFromSession = UserIdenetityUtil.getCurrentAccessToken(request);
-        //
-        // if (null == currentUserIdFromSession) {
-        // return false;
-        // }
-        //
-        // if (-1 != requestURI.indexOf("/mobile")) {
-        // String accessTokenFromRequest = request.getParameter(ApplicationConstant.ACCESS_TOKEN);
-        // if (Objects.equals(accessTokenFromRequest, accessTokenFromSession)) {
-        // return true;
-        // }
-        // } else {
-        // Cookie[] cookies = request.getCookies();
-        // if (null != cookies) {
-        // for (Cookie cookie : cookies) {
-        // if (ApplicationConstant.ACCESS_TOKEN.equals(cookie.getName())) {
-        // String accessTokenFromRequest = cookie.getValue();
-        // if (Objects.equals(accessTokenFromRequest, accessTokenFromSession)) {
-        // return true;
-        // }
-        // }
-        //
-        // }
-        // }
-        //
-        // }
-        //
-        // log.debug("denied this path {}  ", requestURI);
-        // return false;
+         
     }
 
     @Override
@@ -128,7 +64,8 @@ public class UserSessionInterceptor implements HandlerInterceptor {
         // System.out.println("After completion handle");
     }
 
-    public static void setNoNeedLoginURLs(List<String> noNeedLoginURLs) {
-        UserSessionInterceptor.noNeedLoginURLs = noNeedLoginURLs;
-    }
+	public void setNoNeedLoginURLs(List<String> noNeedLoginURLs) {
+		this.noNeedLoginURLs = noNeedLoginURLs;
+	}
+  
 }
