b4c264f3123c3bc155347f18ac0a5974e8287a4a
vavi <ghjapple@163.com>
2015-07-11 17:54:50 +0800

    workaround for bug

--- web/src/main/java/com/xinran/springmvc/interceptor/UserSessionInterceptor.java
+++ web/src/main/java/com/xinran/springmvc/interceptor/UserSessionInterceptor.java
@@ -14,58 +14,83 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
 import com.xinran.controller.util.UserIdenetityUtil;
 import com.xinran.exception.AuthorizationException;
 
 public class UserSessionInterceptor implements HandlerInterceptor {
+	private Logger log = LoggerFactory.getLogger(this.getClass());
 
-     private Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	private List<String> noNeedLoginURLs = Lists.newArrayList();
 
-    private  List<String> noNeedLoginURLs ;
+	public UserSessionInterceptor(Logger log, List<String> noNeedLoginURLs) {
+		noNeedLoginURLs.add("/img/");
+		noNeedLoginURLs.add("/js/");
+		noNeedLoginURLs.add("/css/");
+		noNeedLoginURLs.add("/mock/");
+		noNeedLoginURLs.add("/user/session/signIn");
+		noNeedLoginURLs.add("/user/session/signUp");
+		noNeedLoginURLs.add("/book/search");
+		noNeedLoginURLs.add("/user/signUp");
+		noNeedLoginURLs.add("/user/signIn");
+		noNeedLoginURLs.add("/book/share/records ");
+		noNeedLoginURLs.add("/book/return/records");
+		noNeedLoginURLs.add("/book/borrow/records");
+		noNeedLoginURLs.add("/book/donate/records");
+		noNeedLoginURLs.add("/book/detail");
+		noNeedLoginURLs.add("/book/location");
+	}
 
 
 
 	@Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String requestURI = request.getRequestURI();
-        
-        log.debug("requestURI is {} ,noNeedLoginURLs is  ", requestURI,JSON.toJSONString(noNeedLoginURLs));
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+		String requestURI = request.getRequestURI();
 
-        if ("/".equals(requestURI) || "/mobile".equals(requestURI)) {
-            return true;
-        }
+		log.debug("requestURI is {} ,noNeedLoginURLs is  ", requestURI,
+				JSON.toJSONString(noNeedLoginURLs));
 
-        for (String url : noNeedLoginURLs) {
-            if (-1 != requestURI.indexOf(url)) {
-                return true;
-            }
-        }
+		if ("/".equals(requestURI) || "/mobile".equals(requestURI)) {
+			return true;
+		}
 
-        // intercept
-        Long currentUserIdFromSession = UserIdenetityUtil.getCurrentUserId(request);
-        if (currentUserIdFromSession == null) {
-            // see http://stackoverflow.com/questions/12713873/spring-3-1-how-do-you-send-all-exception-to-one-page
-            throw new AuthorizationException();
-        } else {
-            return true;
-        }
-         
-    }
+		for (String url : noNeedLoginURLs) {
+			if (-1 != requestURI.indexOf(url)) {
+				return true;
+			}
+		}
 
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-                           ModelAndView modelAndView) throws Exception {
-        // System.out.println("Post-handle");
-    }
+		// intercept
+		Long currentUserIdFromSession = UserIdenetityUtil
+				.getCurrentUserId(request);
+		if (currentUserIdFromSession == null) {
+			// see
+			// http://stackoverflow.com/questions/12713873/spring-3-1-how-do-you-send-all-exception-to-one-page
+			throw new AuthorizationException();
+		} else {
+			return true;
+		}
 
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
-                                                                                                                       throws Exception {
-        // System.out.println("After completion handle");
-    }
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// System.out.println("Post-handle");
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// System.out.println("After completion handle");
+	}
 
 	public void setNoNeedLoginURLs(List<String> noNeedLoginURLs) {
 		this.noNeedLoginURLs = noNeedLoginURLs;
 	}
-  
+
 }
