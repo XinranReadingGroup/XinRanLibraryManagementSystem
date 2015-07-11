aaeacd99c2fa85b09816bb3f4c1ce98f1f903fde
vavi <ghjapple@163.com>
2015-07-11 17:37:50 +0800

    add debug log

--- web/src/main/java/com/xinran/springmvc/interceptor/UserSessionInterceptor.java
+++ web/src/main/java/com/xinran/springmvc/interceptor/UserSessionInterceptor.java
@@ -8,16 +8,19 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 import com.xinran.controller.util.UserIdenetityUtil;
 import com.xinran.exception.AuthorizationException;
 
 public class UserSessionInterceptor implements HandlerInterceptor {
 
-    // private Logger log = LoggerFactory.getLogger(this.getClass());
+     private Logger log = LoggerFactory.getLogger(this.getClass());
 
     private static List<String> noNeedLoginURLs = Lists.newArrayList();
 
@@ -47,6 +50,8 @@ public class UserSessionInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String requestURI = request.getRequestURI();
         
+        log.debug("requestURI is {} ,noNeedLoginURLs is  ", requestURI,JSON.toJSONString(noNeedLoginURLs));
+
         
         
         if ("/".equals(requestURI) || "/mobile".equals(requestURI)) {
